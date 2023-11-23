15de31ef3e76cc0666410f5a09c6a09ee95d69b7
Al Baker <baker.allison.m@gmail.com>
2023-11-22 22:37:22 -0700

    attempt to fix dropdowns

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -79,6 +79,7 @@ searchBtn.addEventListener("click", () => {
     .then((data) => {
       currentExercises = data;
       populateExercises(currentExercises);
+      populateDropdown();
       hideAll();
     });
 });
@@ -95,8 +96,10 @@ function populateExercises(exercises) {
     let div = `<section class="p-4 rounded-md bg-slate-50 my-4 shadow-md"">
                 <div class="flex flex-row justify-between items-center">
                   <h2 class="font-bold text-2xl text-slate-800 mb-4">${exercise.name}</h2>
-                  <button class="btn bg-emerald-600 text-white px-3 py-2 rounded-full" onclick="showDropdown(event)"><i class="fa-solid fa-plus"></i></button>
-                  <select class="listSelection hidden" onchange="addExercise(${idCount})"></select>
+                  <section>
+                    <button class="btn bg-emerald-600 text-white px-3 py-2 rounded-full" onclick="showDropdown(event)"><i class="fa-solid fa-plus"></i></button>
+                    <select class="listSelection" style="display: none;" onchange="addExercise(${idCount})"></select>
+                  </section>
                 </div>
                 <h3 class="font-bold text-lg text-slate-600 mb-2">Difficulty: ${exercise.difficulty}, Type: ${exercise.type}, Muscle: ${exercise.muscle}</h3>
                 <button class="btn bg-slate-900 text-white px-4 py-2 rounded-md" onclick="toggleInstructions(${paragraphId})">View Instructions</button>
@@ -114,17 +117,15 @@ function populateDropdown() {
   fetch("/api/lists")
     .then((res) => res.json())
     .then((data) => {
-      addDropdowns.forEach((dropdown) => {
-        dropdown.innerHTML = "<option value=''>--Add to List--</option>";
+      for(let i=0; i< addDropdowns.length; i++) {
+        addDropdowns[i].innerHTML = "<option value=''>--Add to List--</option>";
         data.forEach((list) => {
           let option = `<option value="${list.id}">${list.name}</option>`;
-          dropdown.insertAdjacentHTML("beforeend", option);
+          addDropdowns[i].appendChild(option);
         });
-      });
+      };
     });
 }
-
-populateDropdown();
 
 function toggleInstructions(paragraphId) {
   paragraphId.classList.toggle("hidden");
@@ -132,8 +133,12 @@ function toggleInstructions(paragraphId) {
 
 function showDropdown(event) {
   let clicked = event.target;
-  let dropdown = clicked.closest("div").lastElementChild;
-  dropdown.classList.toggle("hidden");
+  let dropdown = clicked.closest("section").lastElementChild;
+  if (dropdown.style.display === "none") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
 }
 
 function addExercise(idToAdd) {
